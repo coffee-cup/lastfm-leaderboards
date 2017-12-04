617b6347996f5112e6ec59ca26e3078a7b07bad7
Jake Runzer <jakerunzer@gmail.com>
2017-12-03 17:20:14 -0800

    update metadata and readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lastfm-leaderboards
 
+Music leaderboards powered by [last.fm](https://www.last.fm/). Find [here](https://lastfm.jakerunzer.com).
+
 Created from the [elm-template](https://github.com/coffee-cup/elm-template).
--- metadata/metadata.js
+++ metadata/metadata.js
@@ -5,34 +5,34 @@
  */
 
 const Mustache = require('mustache');
-const fs = require('fs');
-const glob = require('glob');
-const path = require('path');
-const mkdirp = require('mkdirp');
+const fs       = require('fs');
+const glob     = require('glob');
+const path     = require('path');
+const mkdirp   = require('mkdirp');
 
 const outputDir = 'build/';
 
-const template = fs.readFileSync('metadata/template.mustache', 'utf8');
-const getJsonData = (f) => JSON.parse(fs.readFileSync(f, 'utf8'));
-const getPageHtml = (json) => Mustache.render(template, json);
-const saveTo = (f, html) => {
-    mkdirp.sync(path.dirname(f));
-    fs.writeFileSync(f, html);
+const template    = fs.readFileSync('metadata/template.mustache', 'utf8');
+const getJsonData = f => JSON.parse(fs.readFileSync(f, 'utf8'));
+const getPageHtml = json => Mustache.render(template, json);
+const saveTo      = (f, html) => {
+  mkdirp.sync(path.dirname(f));
+  fs.writeFileSync(f, html);
 };
 
 glob('metadata/**/*.json', (err, files) => {
-    if (err) throw new Error(err);
+  if (err) throw new Error(err);
 
-    files.forEach((f) => {
-        const json = getJsonData(f);
-        const html = getPageHtml(json);
+  files.forEach(f => {
+    const json = getJsonData(f);
+    const html = getPageHtml(json);
 
-        const name = f.replace('.json', '').replace('metadata/', '');
-        if (path.basename(f) === 'index.json') {
-            saveTo(`${outputDir}${name}.html`, html);
-            saveTo(`public/${name}.html`, html);
-        } else {
-            saveTo(`${outputDir}${name}/index.html`, html);
-        }
-    });
+    const name = f.replace('.json', '').replace('metadata/', '');
+    if (path.basename(f) === 'index.json') {
+      saveTo(`${outputDir}${name}.html`, html);
+      saveTo(`public/${name}.html`, html);
+    } else {
+      saveTo(`${outputDir}${name}/index.html`, html);
+    }
+  });
 });
